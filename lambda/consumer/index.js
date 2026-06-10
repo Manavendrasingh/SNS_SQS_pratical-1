dec3f31dd5a29b189d00dcf60818f7521813fe08
Manvendra singh <53317299+Manavendrasingh@users.noreply.github.com>
2026-06-11 01:01:50 +0530

    Update index.js

--- lambda/consumer/index.js
+++ lambda/consumer/index.js
@@ -11,6 +11,11 @@ exports.handler = async (event = {}) => {
       body: message,
     });
   }
+  let i = 0;
+  while(i>=0){
+      i++;
+      console.log('Processing message:', i);
+  }
 
   return {
     batchItemFailures: [],
